ccb69a5482290d15f700311cb306af371c6435d2
jeeban <jeebanjyoti193@gmail.com>
2023-07-13 09:44:55 +0530

    final commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter marks in pre: ");
+            int preMarks = int.Parse(Console.ReadLine());
+
+            if (preMarks > 60)
+            {
+                Console.Write("Enter marks in final: ");
+                int finalMarks = int.Parse(Console.ReadLine());
+
+                if (finalMarks > 55)
+                {
+                    Console.WriteLine("Result: Selected");
+                }
+                else
+                {
+                    Console.WriteLine("Result: Fail in final");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Result: Fail in pre");
+
+            }
+        }
+    }
+}
